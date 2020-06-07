7119457fb5b48a3cb6d4bdd69f907145a228b651
yuqiang <yuqiang@tuya.com>
2020-06-07 16:43:10 +0800

    ssfsdfs

---
+++ src/main/java/com/micro/mall/config/CodeGenerator.java
@@ -0,0 +1,135 @@
+package com.micro.mall.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:代码生成器演示
+ */
+public class CodeGenerator {
+
+    final static String DB_URL = "jdbc:mysql://127.0.0.1:3306/mall?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+    final static String DB_USERNAME = "root";
+    final static String DB_PASSWORD = "root123456";
+//    final static String OUT_DIR = "D://自动生成结果//";
+
+//    final static String OUT_DIR = "/Users/ohyeah/Desktop/generator/";
+//    final static String OUT_DIR = "E:/01_work/dailuobo/wms-webapi/src/main/java";
+
+    //final static String OUT_DIR = "E:\\Projects\\wms-webapi\\src\\main\\java";
+
+
+    final static String OUT_DIR = "/Users/yuqiang/IdeaProjects";
+
+    //final static String OUT_DIR = "E:\\01_work";
+
+
+    // #### 定义生成类的前缀，生成后会自动去除前缀
+    final static String[] CLASS_PREFIX = new String[]{"bas_"};
+
+    // 业务模块 包名
+    final static String modulePackage = "inv";
+
+    final static String[] GENERATOR_TABLE = new String[]{"bas_sku"};
+
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+        // 选择 freemarker 引擎，默认 Veloctiy
+        // mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(OUT_DIR);
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+        //gc.setKotlin(true);//是否生成 kotlin 代码
+        //gc.setAuthor("");
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        // gc.setMapperName("%sDao");
+        // gc.setXmlName("%sDao");
+        gc.setServiceName("%sDomain");
+        gc.setServiceImplName("%sDomainImpl");
+        gc.setControllerName("%sService");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername(DB_USERNAME);
+        dsc.setPassword(DB_PASSWORD);
+        dsc.setUrl(DB_URL);
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
+        strategy.setTablePrefix(CLASS_PREFIX);// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(GENERATOR_TABLE); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        // 自定义实体，公共字段
+        strategy.setSuperEntityColumns(new String[]{"created_time", "created_user_id", "updated_time", "updated_user_id", "version"});
+        // 自定义 mapper 父类
+        strategy.setSuperMapperClass("com.baomidou.mybatisplus.core.mapper.BaseMapper");
+        // 自定义 service 父类
+        strategy.setSuperServiceClass("com.baomidou.mybatisplus.extension.service.IService");
+        // 自定义 service 实现类父类
+        strategy.setSuperServiceImplClass("com.baomidou.mybatisplus.extension.service.impl.ServiceImpl");
+        // 自定义 controller 父类
+        //strategy.setSuperControllerClass("com.airboard.core.base.BaseController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // .setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        //strategy.setEntityBuilderModel(true);
+        // 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
+        strategy.setEntityLombokModel(true);
+        // Boolean类型字段是否移除is前缀处理
+        strategy.setEntityBooleanColumnRemoveIsPrefix(false);
+        // .setRestControllerStyle(true)
+        // .setControllerMappingHyphenStyle(true)
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.micro.mall.webapi");
+        pc.setController("service." + modulePackage);
+        pc.setEntity("data.model." + modulePackage);
+        pc.setService("domain." + modulePackage);
+        pc.setServiceImpl("domain." + modulePackage + ".impl");
+        pc.setMapper("data.mapper." + modulePackage);
+        pc.setXml("mapper." + modulePackage);
+        mpg.setPackageInfo(pc);
+
+        // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】  ${cfg.abc}
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+                this.setMap(map);
+            }
+        };
+        // 执行生成
+        mpg.execute();
+        // 打印注入设置【可无】
+//        System.err.println(mpg.getCfg().getMap().get("abc"));
+    }
+
+}
