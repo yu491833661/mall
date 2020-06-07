d40204e7102ff350ddff677b08a4695f355c2739
yuqiang <yuqiang@tuya.com>
2020-06-07 17:21:23 +0800

    数据库类

--- src/main/java/com/micro/mall/controller/ProductFileService.java
+++ src/main/java/com/micro/mall/controller/ProductFileService.java
@@ -1,4 +1,4 @@
-package com.micro.mall.service;
+package com.micro.mall.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Controller;
 
 /**
  * <p>
- * 商品详情表 前端控制器
+ * 商品详情-附件表 前端控制器
  * </p>
  *
  * @author ${author}
  * @since 2020-06-07
  */
 @Controller
-@RequestMapping("/productDetail")
-public class ProductDetailService {
+@RequestMapping("/productFile")
+public class ProductFileService {
 
 }
 
--- src/main/java/com/micro/mall/service/impl/ProductDetailDomainImpl.java
+++ src/main/java/com/micro/mall/service/impl/ProductDetailDomainImpl.java
@@ -1,8 +1,8 @@
-package com.micro.mall.domain.impl;
+package com.micro.mall.service.impl;
 
 import com.micro.mall.data.model.ProductDetail;
 import com.micro.mall.data.mapper.ProductDetailMapper;
-import com.micro.mall.domain.ProductDetailDomain;
+import com.micro.mall.service.ProductDetailDomain;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
